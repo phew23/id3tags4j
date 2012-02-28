4fa88ba43a66341094708ce93c4cfcf680742261
phew <phew@phew-laptop.fritz.box>
2012-02-28 14:58:10 +0100

    Changed the LICENSE header.

--- src/in/pussykill/id3/v2/v0_0/ID3v200TagHeader.java
+++ src/in/pussykill/id3/v2/v0_0/ID3v200TagHeader.java
@@ -1,21 +1,18 @@
 /*
- * This file is part of id3tags4j, a library for easy ID3 tag handling for Java.
+ * This file is part of id3tags4j, an ID3 tag library for Java.
  *
- * id3tags4j is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation (FSF).
- * Either version 3 of the License, or (at your option) any later version.
- * 
- * id3tags4j is distributed in the hope that it will be useful for somebody at 
- * some point, but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Neither the author nor 
- * anybody related to this project can be held responsible for any inconvenience 
- * or failure caused by using this open source library. See the GNU General 
- * Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
- * id3tags4j; if not, write to the Free Software Foundation (FSF), Inc., 59 
- * Temple Palace, Suite 330, Boston, MA  02111-1307  USA.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package in.pussykill.id3.v2.v0_0;
 
--- src/in/pussykill/id3/v2/v3_0/ID3v230TagHeader.java
+++ src/in/pussykill/id3/v2/v3_0/ID3v230TagHeader.java
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of id3tags4j, an ID3 tag library for Java.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package in.pussykill.id3.v2.v3_0;
 
