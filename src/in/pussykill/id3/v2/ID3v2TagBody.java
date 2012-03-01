d3039c976e75a148432089d165655cbc9f8f3aa6
phew <phew@phew-laptop.fritz.box>
2012-03-01 05:09:17 +0100

    Added TODO so its shown in the Tasks tab in my IDE.

--- src/in/pussykill/id3/v2/ID3v2TagBody.java
+++ src/in/pussykill/id3/v2/ID3v2TagBody.java
@@ -24,7 +24,7 @@ public class ID3v2TagBody {
     
     private final ID3v2Frame[] frames;
    
-    //TODO
+    //TODO:
     //this is to be initialized with MP3Utilities.parseFrames() or something
     //similiar. ID3v2TagBody body = new ID3v2TagBody(
     //MP3Utilities.parseFrames(bytes));
