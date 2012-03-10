58f304b7c0a5bc13baddf813cceaef95d7bf4c3f
phew <phew@phew-laptop.fritz.box>
2012-03-10 03:15:08 +0100

    Did a first test, successfully read text frames of a MP3 file with ID3v2 v3.0 tag.

--- src/in/pussykill/main/Main.java
+++ src/in/pussykill/main/Main.java
@@ -17,6 +17,9 @@
 package in.pussykill.main;
 
 import in.pussykill.id3.v2.ID3v2Tag;
+import in.pussykill.id3.v2.frames.ID3v2Frame;
+import in.pussykill.id3.v2.frames.ID3v2FrameBody;
+import in.pussykill.id3.v2.frames.ID3v2TextFrameBody;
 import in.pussykill.mp3.MP3File;
 import in.pussykill.mp3.MP3Utilities;
 import java.io.FileNotFoundException;
@@ -31,8 +34,17 @@ public class Main {
     public static void main(String... args) {
         try {
             
-            MP3File mp3 = MP3Utilities.init("files/c.mp3");
+            MP3File mp3 = MP3Utilities.init("files/a.mp3");
             ID3v2Tag id3v2 = mp3.getID3v2Tag();
+            ID3v2Frame[] id3v2Frames = id3v2.getID3v2TagBody().getID3v2Frames();
+            for(ID3v2Frame frame : id3v2Frames) {
+                ID3v2FrameBody id3v2FrameBody = frame.getID3v2FrameBody();
+                if(id3v2FrameBody instanceof ID3v2TextFrameBody) {
+                    ID3v2TextFrameBody id3v2TextFrameBody = (ID3v2TextFrameBody) id3v2FrameBody;
+                    System.out.println(frame.getID3v2FrameHeader().getIdentifier() + ": " + id3v2TextFrameBody.getText());
+                }
+            }
+            
             System.out.println(id3v2.getID3v2TagHeader().getVersion());
            
             
