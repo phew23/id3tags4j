2987834fbc5bfd38b5643b0cb19b80a18a4e9d2d
phew <phew@phew-laptop.fritz.box>
2012-03-13 02:53:56 +0100

    Implemented different methods to be able to excerp ID3v2 frames matching a certain pattern.

--- src/in/pussykill/id3/v2/ID3v2TagBody.java
+++ src/in/pussykill/id3/v2/ID3v2TagBody.java
@@ -17,6 +17,8 @@
 package in.pussykill.id3.v2;
 
 import in.pussykill.id3.v2.frames.ID3v2Frame;
+import in.pussykill.id3.v2.frames.selectors.ID3v2FrameSelector;
+import java.util.Arrays;
 
 /**
  * This class represents all ID3v2 tag bodies.
@@ -40,5 +42,54 @@ public class ID3v2TagBody {
     public ID3v2Frame[] getID3v2Frames() {
         return frames;
     }
+    
+    /**
+     * @param selector {@link ID3v2FrameSelector} to select certain from the 
+     *                 ID3v2 tag body.
+     * @return All frames matching the {@link ID3v2FrameSelector}'s criterias.
+     */
+    public ID3v2Frame[] getID3v2Frames(ID3v2FrameSelector selector) {
+        ID3v2Frame[] selectedFrames = new ID3v2Frame[0];
+        for(ID3v2Frame frame : frames) {
+            if(selector.wants(frame)) {
+                selectedFrames = Arrays.copyOf(selectedFrames, 
+                        selectedFrames.length + 1);
+                selectedFrames[selectedFrames.length - 1] = frame;
+            }
+        }
+        return selectedFrames;
+    }
+    
+    /**
+     * @param identifier Identifier of the wanted {@link ID3v2Frame}.
+     * @return The first {@link ID3v2Frame} found matching the identifier;
+     *         null if no frame matches the identifier.
+     */
+    public ID3v2Frame getID3v2FrameByIdentifier(String identifier) {
+        for(ID3v2Frame frame : frames) {
+            if(frame.getID3v2FrameHeader().getIdentifier().equals(identifier)) {
+                return frame;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * @param identifier Identifier of the wanted {@link ID3v2Frame}s.
+     * @return An {@link ID3v2Frame}[] of all frames matching against the
+     *         identifier.
+     */
+    public ID3v2Frame[] getID3v2FramesByIdentifier(String identifier) {
+        ID3v2Frame[] id3v2Frames = new ID3v2Frame[0];
+        for(ID3v2Frame frame : frames) {
+            if(frame.getID3v2FrameHeader().getIdentifier().equals(identifier)) {
+                id3v2Frames = Arrays.copyOf(id3v2Frames, 
+                        id3v2Frames.length + 1);
+                id3v2Frames[id3v2Frames.length - 1] = frame;
+            }
+        }
+        return id3v2Frames;
+    }
+    
     
 }
