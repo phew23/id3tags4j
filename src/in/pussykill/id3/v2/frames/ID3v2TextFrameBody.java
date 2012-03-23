bdca868caa72febf0937623ae394ef58945cb4fd
phew <phew@phew-laptop.fritz.box>
2012-03-23 14:35:52 +0100

    Added a setter for the information field for testing purposes. This is working virtually so far but cannot write frames physically yet.

--- src/in/pussykill/id3/v2/frames/ID3v2TextFrameBody.java
+++ src/in/pussykill/id3/v2/frames/ID3v2TextFrameBody.java
@@ -16,8 +16,8 @@
  */
 package in.pussykill.id3.v2.frames;
 
-import in.pussykill.id3.v2.ID3v2Constants;
 import in.pussykill.id3.v2.ID3v2Utilities;
+import java.util.Arrays;
 
 /**
  * This class represents all ID3v2 text frame bodies.
@@ -25,8 +25,8 @@ import in.pussykill.id3.v2.ID3v2Utilities;
  */
 public class ID3v2TextFrameBody extends ID3v2FrameBody {
     
-    private final byte encoding;
-    private final byte[] information;
+    private byte encoding;
+    private byte[] information;
     
     public ID3v2TextFrameBody(final byte[] b, final byte encoding, 
             final byte[] information) {
@@ -43,16 +43,32 @@ public class ID3v2TextFrameBody extends ID3v2FrameBody {
     }
     
     /**
-     * @return The text read from this ID3v2 text frame body.
+     * @return The actual text read from this ID3v2 text frame body.
      */
-    public String getText() {
+    public String getInformation() {
         return ID3v2Utilities.getString(information, encoding);
+    }
+    
+    /**
+     * @param information The new text for this text frame body.
+     */
+    public void setInformation(byte[] information, byte encoding) {
+        this.encoding = encoding;
+        this.information = information;
+        
+        //update the super class ID3v2FrameBody with the new data
+        byte[] buffer = new byte[] { encoding };
+        for(byte b : information) {
+            buffer = Arrays.copyOf(buffer, buffer.length + 1);
+            buffer[buffer.length - 1] = b;
+        }
+        super.setBytes(buffer);
     }
     
     @Override
     public String toString() {
         return this.getClass().getSimpleName() + "=[encoding=" + getEncoding() +
-                ", text=" + getText() + "]";
+                ", text=" + getInformation() + "]";
     }
     
 }
